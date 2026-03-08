72df211bae3f59aafe92d3b530340e63f78c36f9
hagay kohelet <hagay2506@gmail.com>
2026-03-08 23:17:54 +0200

    add a image route for check upload image

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import authRoute from './routes/auth.js';
 import reportRoute from './routes/reports.js';
+import { upload } from './utils/handle_image.js';
 
 const app = express()
 const PORT = 3000
@@ -11,7 +12,13 @@ app.use(express.json())
 app.use(cors())
 app.use("/auth", authRoute)
 app.use('/reports', reportRoute)
-
+// app.post('/image', upload.single("image"), (req, res, next) => {
+//     const image = req.file
+//     if (!image) {
+//         return res.status(400).json({ error: "no image upload" })
+//     }
+//     console.log(image)
+// })
 
 app.listen(PORT, () => {
     console.log("server running...")
---
+++ server/utils/handle_image.js
@@ -0,0 +1,15 @@
+import multer from 'multer'
+const storage = multer.memoryStorage()
+const filefilter = (req, file, cb) => {
+    const allowExtensions = ["image/png", "image/jpg", "image/jpeg"];
+    if (allowExtensions.includes(file.mimetype)) {
+        cb(null, true)
+    }
+    else {
+        cb(null, false)
+    }
+
+}
+
+const upload = multer({ storage, filefilter })
+export {upload, filefilter}
